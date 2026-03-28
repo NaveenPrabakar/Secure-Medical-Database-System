af5875d04c2f23654f69fcc04a2bac015470c10f
Naveen Prabakar <nvnprabakar@gmail.com>
2026-03-28 16:06:56 -0500

    intial handler

--- Backend/app/api/v1/router.py
+++
@@ -1,7 +0,0 @@
-﻿from fastapi import APIRouter
-from app.api.v1.endpoints import auth, patients, audit
-
-api_router = APIRouter()
-api_router.include_router(auth.router, prefix='/auth', tags=['auth'])
-api_router.include_router(patients.router, prefix='/patients', tags=['patients'])
-api_router.include_router(audit.router, prefix='/audit', tags=['audit'])
